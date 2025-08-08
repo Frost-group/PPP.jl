9b82bf616c9a95d4d70922c174c44dd9a9081a0c
Jarvist Moore Frost <jarvist@gmail.com>
2025-08-08 16:08:42 +0100

    OK, doesn't look good, I think its just using Gaussian overlaps.

--- src/Fermi.jl
+++ src/Fermi.jl
@@ -2,7 +2,9 @@
 
 using PPP
 using Fermi
+
 using LinearAlgebra
+using Printf
 
 """
     to_fermi_rhf(system::PPP.MolecularSystem, scf_result::PPP.SCFResult)
@@ -53,8 +55,9 @@ function to_fermi_rhf(system::PPP.MolecularSystem, scf_result::PPP.SCFResult)
         multiplicity=multiplicity,
     )
 
-    # MO data from PPP SCF
-    mo_energies = scf_result.energies
+    # MO data from PPP SCF (convert eV → Hartree for Fermi)
+    ev2au = 1 / 27.21138505
+    mo_energies = scf_result.energies .* ev2au
     mo_coefficients = scf_result.eigenvectors
 
     # Fermi.Orbitals.RHFOrbitals expects:
@@ -63,19 +66,19 @@ function to_fermi_rhf(system::PPP.MolecularSystem, scf_result::PPP.SCFResult)
         molecule,
         "sto-3g",
         mo_energies,
-        scf_result.total_energy,
+        scf_result.total_energy * ev2au,
         mo_coefficients,
     )
 
-    hf_energy = scf_result.total_energy
+    hf_energy = scf_result.total_energy * ev2au
     return Fermi.HartreeFock.RHF(molecule, hf_energy, ndocc, nvir, orbitals, 0.0, 0.0)
 end
 
 # Example usage (uncomment to run manually):
 using PPP
-sys, huckel, scf = PPP.run_ppp_calculation("molecules/pyrrole.xyz", PPP.Bedogni2024ModelParams())
+sys, huckel, scf = PPP.run_ppp_calculation("molecules/ethene-2C.xyz", PPP.Bedogni2024ModelParams())
 wfn = to_fermi_rhf(sys, scf)
-@energy wfn => mp2
+mp2 = @energy wfn => mp2
 
 # CCSD: create integral helpers explicitly (does not dispatch on RHF)
 moints = Fermi.Integrals.IntegralHelper{Float64}(orbitals=wfn.orbitals)
@@ -86,3 +89,34 @@ display(ccsd)
 # FCI: API expects (aoints, rhf)
 fci = Fermi.ConfigurationInteraction.RFCI(aoints, wfn, Fermi.ConfigurationInteraction.get_rfci_alg())
 display(fci)
+
+Ha2eV = 27.21138505
+# Compare total and correlation energies across methods
+println("\nTotal Energies (eV):")
+println("Huckel: ", huckel.total_energy)
+println("PPP: ", scf.total_energy)
+
+println("SCF:  ", wfn.energy * Ha2eV)
+println("MP2:  ", mp2.energy * Ha2eV)
+println("CCSD: ", ccsd.energy * Ha2eV)
+println("FCI:  ", fci.energy * Ha2eV)
+
+println("\nCorrelation Energies (eV):")
+println("MP2:  ", mp2.energy * Ha2eV - wfn.energy * Ha2eV)
+println("CCSD: ", ccsd.energy * Ha2eV - wfn.energy * Ha2eV) 
+println("FCI:  ", fci.energy * Ha2eV - wfn.energy * Ha2eV)
+
+
+#Reference values courtesy of PlotDigitizer, lightly edited. Fig 4d: 2T-7N at different levels of theory
+
+#singlets=cis_tda_singlet(wfn; nroots=5)
+#triplets=cis_tda_triplet(wfn; nroots=5)
+#println("TDA-CIS singlet roots (eV): ", singlets)
+#println("TDA-CIS triplet roots (eV): ", triplets)
+   
+# Copy paste so we can see how close we are getting. 
+#@printf("T1: %.4f T1(Bedogni2024): %.4f diff: %.4f\n", triplets[1], 3.428550679531072, triplets[1] - 3.428550679531072)
+#@printf("S1: %.4f S1(Bedogni2024): %.4f diff: %.4f\n", singlets[1], 4.052910052910053, singlets[1] - 4.052910052910053)
+#@printf("T3: %.4f T3(Bedogni2024): %.4f diff: %.4f\n", triplets[3], 3.947089947089947, triplets[3] - 3.947089947089947)
+#@printf("S2: %.4f S2(Bedogni2024): %.4f diff: %.4f\n", singlets[2], 5.216931216931217, singlets[2] - 5.216931216931217)
+
