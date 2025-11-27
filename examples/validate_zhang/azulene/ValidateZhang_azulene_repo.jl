9d6f72d88413c1af3a8d50fb7660c1dc6fbe35d8
abilevi5 <al3721@ic.ac.uk>
2025-11-27 14:20:45 +0000

    test scripts for PPP + CI calculations for azulene

---
+++ examples/validate_zhang/azulene/ValidateZhang_azulene_repo.jl
@@ -0,0 +1,20 @@
+using Test
+using PPP
+using Printf
+using StaticArrays
+using CSV, DataFrames
+
+#Calculate ground state energy for Ethene with Zhang 2011 parameters, also run CIS calculations for each bond length
+
+#NOT FINISHED!!!!!!
+
+# Run one PPP calculation for CIS calculation
+azulene_system, huckel_result, scf_result_azulene = PPP.run_ppp_calculation("molecules/azulene.xyz", Zhang2011Model())
+
+singlet, triplet = run_cis_calculation(azulene_system, scf_result_azulene)
+
+println("First singlet excitation energy (eV): ", singlet.energies[1])
+println("First triplet excitation energy (eV): ", triplet.energies[1])
+
+ST_gap = singlet.energies[1] - triplet.energies[1]
+println("Singlet-Triplet gap (eV): ", ST_gap)
