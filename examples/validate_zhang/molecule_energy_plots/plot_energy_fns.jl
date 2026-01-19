bce150efd15467840fe9178fa2e46c1078827986
abilevi5 <abigail.levison21@imperial.ac.uk>
2026-01-19 14:37:31 +0000

    implemented function to plot energies on their own, separate to function that plots ST gaps

--- examples/validate_zhang/molecule_energy_plots/plot_energy_fns.jl
+++ examples/validate_zhang/molecule_energy_plots/plot_energy_fns.jl
@@ -98,6 +98,9 @@ function plot_ST_comparison(orca_data::Vector{MoleculeSTData}, ci_data::Vector{M
     ci_ST_data = [(isa(ci_data[i].ST_gap, Float64) ? ci_data[i].ST_gap : NaN) for i in 1:n_molecules]
     zhang_ST_data = [(isa(zhang_data[i].ST_gap, Float64) ? zhang_data[i].ST_gap : NaN) for i in 1:n_molecules]
 
+    test_vcat = vcat(orca_ST_data, ci_ST_data, zhang_ST_data)
+    max_value = maximum(filter(!isnan, test_vcat))
+
     x = 1:n_molecules
 
     xtic_labels = "set xtics (" *
@@ -106,9 +109,9 @@ function plot_ST_comparison(orca_data::Vector{MoleculeSTData}, ci_data::Vector{M
 
     @gp(
         "set term pdfcairo enhanced size 6in,5in",
-        "set output '../../chem/zhang_plots/ST_gap_comparison_all3_updated.pdf'",
+        "set output '../zhang_plots/ST_gap_comparison_all3_updated.pdf'",
         "set xrange [0:" * string(n_molecules + 1) * "]",
-        "set yrange [0:" * string(maximum(orca_ST_data)+ maximum(orca_ST_data)*0.1) * "]",
+        "set yrange [0:" * string(max_value + max_value*0.1) * "]",
         #"set xtics (" * join(["'$(molecule_names[i])' " * string(i) for i in 1:n_molecules], ", ") * ") rotate by 90 right",
         "set grid",
         xtic_labels,
@@ -116,9 +119,78 @@ function plot_ST_comparison(orca_data::Vector{MoleculeSTData}, ci_data::Vector{M
         "set ytics 0, 0.5, 5.0",
         "set ylabel 'Singlet-Triplet Gap (eV)'",
         "set title 'Singlet-Triplet Gap Comparison: ORCA vs PPP.jl vs Zhang et al.'",
-        x, orca_ST_data, "title 'ORCA'",
-        x, ci_ST_data, "title 'PPP-SCF + CIS'",
-        x, zhang_ST_data, "title 'Zhang 2011 results'",
+
+        "set linestyle 1 lc rgb '#2b8cbe' ps 1.2",  #blue
+        "set linestyle 2 lc rgb '#31a354' ps 1.2",  #green
+        "set linestyle 3 lc rgb '#c51b8a' ps 1.2",  #purple
+
+
+        x, orca_ST_data, "w p ls 1 title 'ORCA'",
+        x, ci_ST_data, "w p ls 2 title 'PPP-SCF + CIS'",
+        x, zhang_ST_data, "w p ls 3 title 'Zhang 2011 results'",
+    )
+    @gp "set output"
+end
+
+function plot_energies_comparison(orca_data::Vector{MoleculeSTData}, ci_data::Vector{MoleculeSTData}, zhang_data::Vector{MoleculeSTData})
+    n_molecules = length(orca_data)
+    molecule_names = ["$(orca_data[i].name)" for i in 1:n_molecules]
+    #convert missing or nothing to NaN for plotting
+    orca_singlet_data = [(isa(orca_data[i].singlet_E1, Float64) ? orca_data[i].singlet_E1 : NaN) for i in 1:n_molecules]
+    # println(orca_singlet_data)
+    orca_triplet_data = [(isa(orca_data[i].triplet_E1, Float64) ? orca_data[i].triplet_E1 : NaN) for i in 1:n_molecules]
+    # println(orca_triplet_data)
+
+    ci_singlet_data = [(isa(ci_data[i].singlet_E1, Float64) ? ci_data[i].singlet_E1 : NaN) for i in 1:n_molecules]
+    # println(ci_singlet_data)
+    ci_triplet_data = [(isa(ci_data[i].triplet_E1, Float64) ? ci_data[i].triplet_E1 : NaN) for i in 1:n_molecules]
+    # println(ci_triplet_data)
+
+    zhang_singlet_data = [(isa(zhang_data[i].singlet_E1, Float64) ? zhang_data[i].singlet_E1 : NaN) for i in 1:n_molecules]
+    # println(zhang_singlet_data)
+    zhang_triplet_data = [(isa(zhang_data[i].triplet_E1, Float64) ? zhang_data[i].triplet_E1 : NaN) for i in 1:n_molecules]
+    # println(zhang_triplet_data)
+ 
+    zhang_ST_data = [(isa(zhang_data[i].ST_gap, Float64) ? zhang_data[i].ST_gap : NaN) for i in 1:n_molecules]
+    # println(zhang_ST_data)
+
+    x = 1:n_molecules
+    test_vcat = vcat(orca_singlet_data, orca_triplet_data, ci_singlet_data, ci_triplet_data, zhang_singlet_data, zhang_triplet_data)
+    max_value = maximum(filter(!isnan, test_vcat))
+    # println("Max value: $max_value")
+    
+    xtic_labels = "set xtics (" *
+    join(["\"$(molecule_names[i])\" " * string(i) for i in 1:n_molecules], ", ") *
+    ") rotate by 90 right"
+
+    @gp(
+        "set term pdfcairo enhanced size 6in,5in",
+        "set output '../zhang_plots/energy_comparison_all3_updated.pdf'",
+        "set xrange [0:" * string(n_molecules + 1) * "]",
+        "set yrange [0:" * string(max_value + max_value*0.1) * "]",
+        "set grid",
+        xtic_labels,
+        "set xtics rotate by 45 right",
+        "set ytics 0, 0.5, 5.0",
+        "set ylabel 'Excitation Energy (eV)'",
+        "set title 'Singlet and Triplet Excitation Energy: ORCA vs PPP.jl vs Zhang et al.'",
+
+        "set linestyle 1 lc rgb '#2b8cbe' ps 1.2",  #blue
+        "set linestyle 2 lc rgb '#31a354' ps 1.2",  #green
+        "set linestyle 3 lc rgb '#c51b8a' ps 1.2",  #purple
+
+        "set label 'ORCA' at screen 0.24,0.874 font ',15' left tc ls 1",
+        "set label 'PPP-SCF + CIS' at screen 0.24,0.82225 font ',15' left tc ls 2",
+        "set label 'Zhang 2011' at screen 0.24,0.8515 font ',15' left tc ls 3",
+
+        "set key font \",15\"",
+        x, orca_singlet_data, "w p pt 2 ps 1.2 lc rgb '#2b8cbe' title 'singlet'",  #blue
+        x, orca_triplet_data, "w p pt 1 ps 1.2 lc rgb '#2b8cbe' title 'triplet'",
+        x, ci_singlet_data, "w p pt 2 ps 1.2 lc rgb '#31a354' notitle", #green
+        x, ci_triplet_data, "w p pt 1 ps 1.2 lc rgb '#31a354' notitle",
+        x, zhang_singlet_data, "w p pt 2 ps 1.2 lc rgb '#c51b8a' notitle", #purple
+        x, zhang_triplet_data, "w p pt 1 ps 1.2 lc rgb '#c51b8a' notitle",
+        #x, zhang_ST_data, "w p pt 8 lc rgb '#d62728' title 'Zhang ST gap'",
     )
     @gp "set output"
 end
@@ -131,3 +203,5 @@ orca_st_data = construct_orca_st_data(df_orca_st)
 zhang_st_data = construct_zhang_st_data(df_zhang_st)
 cis_st_data = generate_ST_with_CI(df_orca_st, PPP.Zhang2011Model())
 plot_ST_comparison(orca_st_data, cis_st_data, zhang_st_data)
+plot_energies_comparison(orca_st_data, cis_st_data, zhang_st_data)
+
